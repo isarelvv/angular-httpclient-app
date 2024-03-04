d68f48ec9d30446e3b010ed5c3e5c12311c72196
appsTorreon <60909559+isarelvv@users.noreply.github.com>
2024-03-04 12:10:36 -0600

    change ip to api

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -15,14 +15,14 @@ export class AuthService {
   constructor(private http: HttpClient) {}
   // User registration
   register(user: User): Observable<any> {
-    return this.http.post('http://127.0.0.1:8000/api/auth/register', user);
+    return this.http.post('http://146.190.52.131:8000/api/auth/register', user);
   }
   // Login
   signin(user: User): Observable<any> {
-    return this.http.post<any>('http://127.0.0.1:8000/api/auth/login', user);
+    return this.http.post<any>('http://146.190.52.131:8000/api/auth/login', user);
   }
   // Access user profile
   profileUser(): Observable<any> {
-    return this.http.get('http://127.0.0.1:8000/api/auth/user-profile');
+    return this.http.get('http://146.190.52.131:8000/api/auth/user-profile');
   }
 }
